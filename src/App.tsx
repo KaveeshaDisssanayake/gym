0637077537aef33013551a562131fb671e814bbb
Kaveesha Dissanayake <90174106+KaveeshaDisssanayake@users.noreply.github.com>
2024-06-19 21:27:24 +0530

    contact us

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { SelectedPage } from "./shared/types";
 import Home from '@/scenes/home';
 import Benefits from '@/scenes/benefits';
 import OurClasses from "./scenes/ourClasses";
+import ContactUs from '@/scenes/contactUs';
 
 
 
@@ -42,6 +43,9 @@ function App() {
      setSelectedPage={setSelectedPage}
      />
      <OurClasses
+     setSelectedPage={setSelectedPage}
+     />
+     <ContactUs
      setSelectedPage={setSelectedPage}
      />
     </div>
